4212ade301d018e9813034462de71eeb9de9346e
dbobkova <das-bobkova@yandex.ru>
2016-12-09 15:11:19 +0400

    Create order collector and Order

--- src/Mian.java
+++ src/Mian.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
 /**
  * Created by Daria Serebryakova on 09.12.2016.
  */
@@ -5,20 +8,14 @@ public class Mian {
 
     public static void main(String[] args) {
 
-        Product product1 = new Pen(2,25.0,1,0.5,"Gel", "red");
-        System.out.println("product1 = " + product1);
-
-        Product product2 = new Pen(1,50.7,3,0.5,"Ball", "red","blue", "black");
-        System.out.println("product2 = " + product2);
-
         Client client1 = new Client("Ivanov I.I", 5);
-        System.out.println("client1 = " + client1);
+        Client client2 = new Client("Petrov A.P", 7);
+        LinkedList<Product> products1 = CollectOrder.orderCollecter();
 
-        Product product3 = new NoteBook(2, 60, "A5", "Yellow", "line", 96);
-        System.out.println("product3 = " + product3);
-        
-        Product product4 = new Cutter(1, 44.68, "Yellow","steel");
-        System.out.println("product4 = " + product4);
+        Order order1 = new Order(client1, products1);
+        System.out.println("order1 = " + order1);
+        Order order2 = new Order(client2, products1);
+        System.out.println("order2 = " + order2);
 
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,26 @@
+import java.util.LinkedList;
+
 /**
  * Created by Daria Serebryakova on 09.12.2016.
  */
 public class Order {
+    private static int numberOfOrder = 0;
+    private Client client;
+    private LinkedList<Product> products;
+
+
+    public Order(Client client, LinkedList<Product> products) {
+        numberOfOrder++;
+        this.client = client;
+        this.products = products;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "numberOfOrder=00" + numberOfOrder+
+                ", client=" + client +
+                ", products=" + products +
+                '}';
+    }
 }
