5f8ba7c61ff7bab6672ac8626f25048e4297a5e0
dbobkova <das-bobkova@yandex.ru>
2016-12-12 18:56:43 +0400

    Collect order changed, new order add,  bug with order number fixed

---
+++ src/savaCollectScan/CollectOrder.java
@@ -0,0 +1,63 @@
+package savaCollectScan;
+import stationeryShop.*;
+import java.util.LinkedList;
+
+/**
+ * Created by Daria Serebryakova on 09.12.2016.
+ */
+public class CollectOrder {
+
+    public static LinkedList<Order> orderCollecter(){
+
+        //Clients
+        Client client1 = new Client("Ivanov O.P", 5);
+        Client client2 = new Client("Anisimova A.P", 7);
+        Client client3 = new Client("Romanova U.R", 5);
+        Client client4 = new Client("Pushkin A.S", 7);
+
+        LinkedList<Client> clients = new LinkedList<>();
+        clients.add(client1);
+        clients.add(client2);
+        clients.add(client3);
+        clients.add(client4);
+        SaveToFile.saveClientToFile(clients);
+
+        //Products for clients
+        LinkedList<Product> products1 = new LinkedList<>();
+        LinkedList<Product> products2 = new LinkedList<>();
+        products1.add(new Pen(2,25.0,1,0.5,"Gel", "red"));
+        products1.add(new Pen(2,25.0,1,0.5,"Gel", "blue"));
+        products2.add(new NoteBook(2, 60, "A5", "Yellow", "line", 96));
+
+        LinkedList<Product> products3 = new LinkedList<>();
+        products3.add(new Cutter(1, 38, "Black", "steel"));
+
+        LinkedList<Product> products4 = new LinkedList<>();
+        products4.add(new Pen(2,25.0,3,0.5,"Gel", "red", "blue", "green"));
+
+        LinkedList<LinkedList<Product>> products = new LinkedList<>();
+        products.add(products1);
+        products.add(products2);
+        products.add(products3);
+        products.add(products4);
+        SaveToFile.saveProductToFile(products);
+
+        //Order = client + product list
+        Order order1 = new Order(client1, products1);
+        Order order2 = new Order(client2, products2);
+        Order order3 = new Order(client3, products3);
+        Order order4 = new Order(client4, products4);
+        LinkedList<Order> orders = new LinkedList<>();
+        orders.add(order1);
+        orders.add(order2);
+        orders.add(order3);
+        orders.add(order4);
+        SaveToFile.saveOrderToFile(orders);
+
+//        for(Order order : orders){
+//            System.out.println(order);
+//        }
+
+        return orders;
+    }
+}
--- src/stationeryShop/Order.java
+++ src/stationeryShop/Order.java
@@ -6,19 +6,25 @@ import java.util.LinkedList;
  * Created by Daria Serebryakova on 09.12.2016.
  */
 public class Order {
-    private static int numberOfOrder = 0;
+    private int numberOfOrder = 0;
+    private static int counter = 1;
     private Client client;
     private LinkedList<Product> products;
 
 
 
     public Order(Client client, LinkedList<Product> products) {
-        numberOfOrder++;
+        numberOfOrder = counter;
+        counter++;
         this.client = client;
         this.products = products;
 
-    }
 
+    }
+//    public static int getCurrNumberOfOrder(){
+//
+//        return numberOfOrder;
+//    }
     @Override
     public String toString() {
         return  numberOfOrder+
