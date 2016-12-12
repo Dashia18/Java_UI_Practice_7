64b786700b8c449beed3148d80258bc55bc0196d
dbobkova <das-bobkova@yandex.ru>
2016-12-12 18:57:09 +0400

    Scan from .txt add

--- src/Mian.java
+++ src/Mian.java
@@ -1,11 +1,9 @@
-import savaAndCollect.SaveClientsToFile;
-import savaAndCollect.SaveOrderToFile;
-import savaAndCollect.SaveProductsToFile;
+import savaCollectScan.CollectOrder;
+import savaCollectScan.ScanTxt;
 import stationeryShop.*;
-
 import java.util.LinkedList;
-
-
+import java.util.Scanner;
+import java.io.File;
 /**
  * Created by Daria Serebryakova on 09.12.2016.
  */
@@ -13,36 +11,13 @@ public class Mian {
 
     public static void main(String[] args) {
 
-        Client client1 = new Client("Ivanov O.P", 5);
-        Client client2 = new Client("Petrov A.P", 7);
-        LinkedList<Client> clients = new LinkedList<>();
-        clients.add(client1);
-        clients.add(client2);
-        SaveClientsToFile.saveToFile(clients);
+        LinkedList<Order> orders = CollectOrder.orderCollecter();
+
+        ScanTxt.txtScaner("data\\clients.txt");
+        
 
 
-        LinkedList<Product> products1 = new LinkedList<>();
-        LinkedList<Product> products2 = new LinkedList<>();
-        products1.add(new Pen(2,25.0,1,0.5,"Gel", "red"));
-        products1.add(new Pen(2,25.0,1,0.5,"Gel", "blue"));
-        products2.add(new NoteBook(2, 60, "A5", "Yellow", "line", 96));
-        LinkedList<LinkedList<Product>> products = new LinkedList<>();
-        products.add(products1);
-        products.add(products2);
-        SaveProductsToFile.saveToFile(products);
 
-
-        Order order1 = new Order(client1, products1);
-        System.out.println("order1 = " + order1);
-        Order order2 = new Order(client2, products2);
-        System.out.println("order2 = " + order2);
-        LinkedList<Order> orders = new LinkedList<>();
-        orders.add(order1);
-        orders.add(order2);
-        SaveOrderToFile.saveToFile(orders);
-
-
-        String str = "Hello";
 
 
 
---
+++ src/savaCollectScan/ScanTxt.java
@@ -0,0 +1,30 @@
+package savaCollectScan;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Created by Daria Serebryakova on 12.12.2016.
+ */
+public class ScanTxt {
+
+    public static void txtScaner(String path) {
+        LinkedList<String> info = new LinkedList<>();
+
+        try {
+            Scanner in = new Scanner(new File(path));
+            StringBuffer data = new StringBuffer();
+            while (in.hasNext())
+                data.append(in.nextLine()).append("\n");
+            info.add(data.toString());
+//                System.out.println(data.toString());
+        } catch ( Exception ex ) {
+            ex.printStackTrace();
+        }
+
+        for(String str : info){
+            System.out.println(str);
+        }
+    }
+}
