028e851e3f04f84937166febd33906d8788b9cef
Dashia <das-bobkova@yandex.ru>
2016-12-23 20:14:41 +0300

    Add methods to separate logic and delete double pieces of code

---
+++ src/uiShop/RadioGroup.java
@@ -0,0 +1,33 @@
+package uiShop;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Daria Serebryakova on 23.12.2016.
+ */
+public class RadioGroup {
+    public static JPanel addRadioGroup(JPanel mainPane,JPanel panelRadio,  ActionListener aL,
+                                       String mainName, String[] buttonsNames) {
+        // Группа связанных радио-переключателей
+//        JPanel panelRadio = new JPanel(new GridLayout(0, 1, 0, 5));
+//        panelRadio.setVisible(false);
+        panelRadio.setBorder(BorderFactory.createTitledBorder(mainName));
+
+        ButtonGroup bg1 = new ButtonGroup();
+        for (int i = 0; i < buttonsNames.length; i++) {
+            JRadioButton radio = new JRadioButton(buttonsNames[i]);
+            panelRadio.add(radio);
+            radio.setFont(new Font("Arial", Font.PLAIN, 18));
+
+                radio.addActionListener(aL);
+
+
+            bg1.add(radio);
+        }
+        mainPane.add(panelRadio);
+        return panelRadio;
+    }
+
+}
---
+++ src/uiShop/ScrolTextArea.java
@@ -0,0 +1,20 @@
+package uiShop;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Daria Serebryakova on 23.12.2016.
+ */
+public class ScrolTextArea {
+    public static void addScrollTextArea(JPanel mainPanel, JTextArea textOut) {
+        textOut.setEditable(false);
+        textOut.setFont(new Font("Arial", Font.PLAIN, 18));
+        textOut.setBorder(BorderFactory.createTitledBorder("file contents:\n" ));
+
+        JScrollPane scroll = new JScrollPane (textOut,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        mainPanel.add(scroll);
+    }
+}
