34712585bec3a7272b0a5548092e5cba8122da99
Dashia <das-bobkova@yandex.ru>
2016-12-23 20:11:45 +0300

    Refactor new package name

--- src/uiShop/actionListeners/ALTextPath.java
+++ src/uiShop/actionListeners/ALTextPath.java
@@ -1,5 +1,7 @@
 package uiShop.actionListener;
 
+import dataScanAndSave.MyScanner;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,11 +10,14 @@ import java.awt.event.ActionListener;
  * Created by Daria Serebryakova on 20.12.2016.
  */
 public class ALTextPath {
-    public static ActionListener  getActionListener(JTextField textPath, JTextArea textOut) {
+    public static ActionListener getActionListener(JTextField textPath, JTextArea textOut) {
         ActionListener aL = new  ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String filePath = textPath.getText();
+                String fileData=  MyScanner.txtScanner(filePath);//    data\clients.txt
                 textOut.append(textPath.getText() + "\n");
+                textOut.append(fileData + "\n");
+
             }
         };
         return aL;
